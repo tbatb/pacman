c2aec69f8af3f93069490e490cc209a1873aaeb3
Tegshbayar Batbayar <tbat@Mac.fritz.box>
2024-11-01 15:39:43 +0100

    added pacman movement

--- backend/src/main/pacman.js
+++ backend/src/main/pacman.js
@@ -2,11 +2,41 @@ class Pacman {
     constructor(x, y, direction, speed, score) {
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
         this.direction = 4;
         this.newDirection = 4;
         this.speed = speed;
         this.frameCount = 7;
         this.score = score;
         this.currentFrameCount = 4;
+    }
+    
+    eat() {
+        
+    }
+    
+    moveLeft2Right(){
+        
+    }
+    
+    moveRight2Left(){
+        
+    }
+    
+    moveUp(){
+        
+    }
+    
+    moveDown(){
+        
+    }
+    
+    changeDirection(){
+        
+    }
+    
+    checkCollisionWithGhost(){
+        
     }
 }
